f0dee96f24590ba18a91ee96d7339fe3e4778b9e
Piyachatchucu <39977523+Piyachatchucu@users.noreply.github.com>
2018-11-15 09:47:19 +0700

    fix some controller

--- Pro/Pro/Controllers/APIS/PromotionAPIController.cs
+++ Pro/Pro/Controllers/APIS/PromotionAPIController.cs
@@ -1,20 +1,21 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Promotion.Models;
 
-//namespace Promotion.Webapp.Controllers.APIS
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class PromotionAPIController : ControllerBase
-//    {
-//    //    [HttpGet]
-//    //    public ActionResult<List<>> GetAll()
-//    //    {
-//    //        return null;
-//    //    }
+namespace Promotion.Controllers.APIS
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PromotionAPIController : ControllerBase
+    {
+        //[HttpGet]
+        //public ActionResult<List<>> GetAll()
+        //{
+        //    return null;
+        //}
 
-//    //}
-//}
+    }
+}
